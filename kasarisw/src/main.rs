f89c846f91b5a264c814c588e03def5f0d91f277
Perttu Ahola <celeron55@gmail.com>
2025-07-20 17:21:55 +0300

    Try to lighten motor modulation CPU load

--- kasarisw/src/main.rs
+++ kasarisw/src/main.rs
@@ -57,7 +57,7 @@ const PWM_HZ: f32 = 400.0;
 
 const RPM_PER_THROTTLE_PERCENT: f32 = 30.0;
 
-const MOTOR_UPDATE_HZ: f32 = 400.0;
+const MOTOR_UPDATE_HZ: f32 = 200.0;
 
 // When you are okay with using a nightly compiler it's better to use https://docs.rs/static_cell/2.1.0/static_cell/macro.make_static.html
 macro_rules! mk_static {
